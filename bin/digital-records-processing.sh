8ac241c7009a115b9c059c3ed7e43a7bd62b56bc
Jarrett Drake <jmdrake@princeton.edu>
2014-11-11 14:15:14 -0500

    initial commit of script for pprocessing digital records

---
+++ bin/digital-records-processing.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#create processing directories
+echo "What is the 
+
+#create destination directories
+
+#run virus scan and log outcomes
+
+#ask whether to disk image
+	#if yes, run guymager and log outcome
+#if no, ask to validate checksums
+	#if yes, run bagger and log outcome
+	#if no, proceed to next step
+
+#run bulkextractor and log outcomes
+
+#run DROID and log outcomes
+
+#inform archivist to create access copy 
+
+#ask archivists if accession contains more files and/or disks
+	#if yes, return to virus scan
+	#if no, proceed to next step
+
+#run bagger and log outcomes
+
